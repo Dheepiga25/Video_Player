c01186fc1210e2f15a962c7d4e2c78cdc83b99c7
Dheepiga25 <71635567+Dheepiga25@users.noreply.github.com>
2020-09-21 16:54:00 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/video_player/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.video_player;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.URLUtil;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    String VIDEO_SAMPLE = "";
+    private VideoView mVideoView;
+    private TextView mTextView;
+    private int mCurrentPosition = 0;
+    private static final String PLAYBACK_TIME = "play_time";
+    int a = 0, b = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVideoView = findViewById(R.id.videoview);
+        mTextView = findViewById(R.id.textview);
+
+        //Volley Request a string response from the provided URL.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://15.207.150.183/API/index.php?p=videoTestAPI";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.contains("mp4Video"))
+                        {    a = response.indexOf("mp4Video");
+                            b = response.lastIndexOf("mp4");
+                        }
+                        VIDEO_SAMPLE = response.substring(a+11,b+3);
+                        VIDEO_SAMPLE = VIDEO_SAMPLE.replaceAll("\\\\", "");
+                        System.out.println("Link "+VIDEO_SAMPLE);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+        //Media Player
+
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controller);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+    }
+
+    private void initializePlayer() {
+        mTextView.setVisibility(VideoView.VISIBLE);
+
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
+
+        mVideoView.setOnPreparedListener(
+                new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+
+                        mTextView.setVisibility(VideoView.INVISIBLE);
+
+                        if (mCurrentPosition > 0) {
+                            mVideoView.seekTo(mCurrentPosition);
+                        } else {
+                            mVideoView.seekTo(1);
+                        }
+
+                        // Start playing
+                        mVideoView.start();
+                    }
+                });
+
+        // runs after media has finished playing.
+        mVideoView.setOnCompletionListener(
+                new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this,"Song complete",Toast.LENGTH_SHORT).show();
+
+                        //the video position to the start.
+                        mVideoView.seekTo(0);
+                    }
+                });
+    }
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
+    }
+
+    private Uri getMedia(String mediaName) {
+        if (URLUtil.isValidUrl(mediaName)) {
+            return Uri.parse(mediaName);
+        } else {
+            return Uri.parse("android.resource://" + getPackageName() +"/raw/" + mediaName);
+        }
+    }
+}
